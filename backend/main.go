fc92735f152ac8e56a5edfa96f4b0f242ce22304
wuyuancheng <960319081@qq.com>
2026-05-18 20:53:10 +0800

    feat: 新增优雅的关闭服务器

--- backend/main.go
+++ backend/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	//注意“exchangeapp”是go.mod文件中定义的模块名
+	"context"
 	"exchangeapp/config" //引入config包
 	"exchangeapp/router"
+	"log"
+
 	//引入gin: go get github.com/gin-gonic/gin
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	//加载配置
 	config.InitConfig()
 	r := router.SetupRouter() //接受router包中SetupRouter函数返回的gin.Engine实例
-	port := config.AppConfig.App.Port
-	if port == "" {
-		port = ":3000" //如果配置文件中没有定义端口，使用默认端口3000
+	srv := &http.Server{
+		Addr:    config.AppConfig.App.Port, //从配置文件中获取端口号
+		Handler: r,
 	}
-	r.Run(config.AppConfig.App.Port) //监听配置文件中定义的端口
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no params) by default sends syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("正在关闭服务器 ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("服务器已关闭")
 }
