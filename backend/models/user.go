b4a27ef6a86b63d1afc818783aae1f8e357ba2a9
wuyuancheng <960319081@qq.com>
2026-05-18 23:55:38 +0800

    feat: 修复了注册的bug

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,22 +4,22 @@ import "gorm.io/gorm"
 
 type User struct {
 	/*
-		GORM提供了一个预定义的结构体，名为gorm.Model，其中包含常用字段：
+			GORM提供了一个预定义的结构体，名为gorm.Model，其中包含常用字段：
 
-		// gorm.Model 的定义
-		type   类型 Model struct {   模型结构{
-	  ID        uint           `gorm:"primaryKey"`
-	  ID        uint           `gorm:"primaryKey"`
+			// gorm.Model 的定义
+			type   类型 Model struct {   模型结构{
+		  ID        uint           `gorm:"primaryKey"`
+		  ID        uint           `gorm:"primaryKey"`
 
-	  CreatedAt time.Time   CreatedAt时间。时间
-	  UpdatedAt time.Time   UpdatedAt时间。时间
-	  DeletedAt gorm.DeletedAt `gorm:"index"`
-	DeletedAt弄脏。DeletedAt“gorm:“index"”
+		  CreatedAt time.Time   CreatedAt时间。时间
+		  UpdatedAt time.Time   UpdatedAt时间。时间
+		  DeletedAt gorm.DeletedAt `gorm:"index"`
+		DeletedAt弄脏。DeletedAt“gorm:“index"”
 
-	}
+		}
 
 	*/
 	gorm.Model
-	Username string `gorm:"unique"` //用户名唯一
-	Password string
+	Username string `gorm:"unique" binding:"required"` //用户名唯一
+	Password string `binding:"required"`
 }
